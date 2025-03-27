a1bf6e240c44688388628bb9bc6877452dd9516c
Samuel <08recsam@rgtfo-me.it>
2025-03-27 09:02:10 +0100

    change2

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,6 @@
 
 #include <Arduino.h>
 
-
-
- 
  #define LED1 5    // Low-side switch (leuchtet mit LOW)
  #define LED2 6    // High-side switch (leuchtet mit HIGH)
  #define TASTER1 3 // HIGH wenn gedrückt
@@ -41,44 +38,14 @@
  #define ANALOGBLINKER_MIN 100     // ms
  #define ANALOGBLINKER_MAX 3000    // ms
  
- // Instanzieren der Strukturen
- button taster1;
- 
- analogblinker a_blinker;
- 
  void setup()
  {
      Serial.begin(115200);
      Serial.println("..Start..\n");
- 
-     // Initialisierung der Strukturen
-     taster1.init(TASTER1, false, DB_ZEIT, LONGPRESSTIME);
-     a_blinker.init(LED1, LED2, BLINKTIME, ANALOGBLINKER_POLLTIME, true, true);
  }
- 
+
  void loop()
  {
-     taster1.poll();
-     a_blinker.poll();
- 
-     a_blinker.setblinktime (((((ANALOGBLINKER_MAX - ANALOGBLINKER_MIN) / 1023.0) * analogRead(POTI)) + 100) / 4);
- 
- 
-     if (taster1.longpress)
-     {
-         a_blinker.setblinktime(2000);
-     }
-     else if (taster1.falling)
-     {
-         a_blinker.enable = !a_blinker.enable;
-         if (a_blinker.enable)
-         {
-             Serial.println("Blinker eingeschaltet");
-         }
-         else
-         {
-             Serial.println("Blinker ausgeschaltet");
-         }
-     }
+
  }
  
