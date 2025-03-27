4815184fad240c815ba0894c611cb02eb1c52404
pippo <08krophi@rgtfo-me.it>
2025-03-27 09:12:53 +0100

    gelosch

--- src/helper.h
+++ src/helper.h
@@ -56,28 +56,6 @@ private:
     möglich eine negation des Ausgangs direkt anzugeben. Die poll-Methode muss
     bei jedem Loop ausgeführt werden.
 */
-class blinker
-{
-public:
-    bool enable = false;
-    uint8_t pin1 = 0;
-    bool neg1 = false;
-    uint8_t pin2 = 0;
-    bool neg2 = false;
-    uint16_t blinkTime = 0;
-    unsigned long lastblink = 0;
-    bool blinkState = false;
-
-    // PinNr.1, Negierung1, PinNr.2, Negierung2, Blinkzeit [ms], Startzustand
-    void init(uint8_t, bool, uint8_t, bool, uint16_t, bool);
-
-    void poll();
-
-    void on();
-    void off();
-    private:
-};
-
 
 
 #endif
